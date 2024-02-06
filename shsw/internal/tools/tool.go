efeee98c4b2eb0d779297f76489efb5f160446d7
Federico Galatolo <galatolo.federico@gmail.com>
2024-02-06 11:11:59 +0100

    better naming

--- shsw/internal/tools/tool.go
+++ shsw/internal/tools/tool.go
@@ -41,55 +41,55 @@ type ToolResult struct {
 	Error    error
 }
 
-func (config *Tool) Run(ch chan<- ToolResult) {
-	result := ToolResult{Name: config.Name, IsLogNew: false, Error: nil}
+func (tool *Tool) Run(ch chan<- ToolResult) {
+	result := ToolResult{Name: tool.Name, IsLogNew: false, Error: nil}
 	//TODO: implement log hash check
-	utils.CheckPathForFile(config.LogFile)
-	config.State.LastRun = time.Now().Format(time.RFC3339)
-	config.State.State = Running
-	err := config.Runner.Run(*config)
+	utils.CheckPathForFile(tool.LogFile)
+	tool.State.LastRun = time.Now().Format(time.RFC3339)
+	tool.State.State = Running
+	err := tool.Runner.Run(*tool)
 
 	if err != nil {
-		config.State.LastError = err.Error()
-		config.State.State = Failed
+		tool.State.LastError = err.Error()
+		tool.State.State = Failed
 		result.Error = err
 	} else {
-		config.State.LastError = ""
-		config.State.State = Ready
+		tool.State.LastError = ""
+		tool.State.State = Ready
 	}
 
-	config.Save()
+	tool.Save()
 	ch <- result
 }
 
-func (config *Tool) Load() {
-	utils.CheckPathForFile(config.StateFile)
-	if _, err := os.Stat(config.StateFile); os.IsNotExist(err) {
-		config.State = ToolState{
+func (tool *Tool) Load() {
+	utils.CheckPathForFile(tool.StateFile)
+	if _, err := os.Stat(tool.StateFile); os.IsNotExist(err) {
+		tool.State = ToolState{
 			LastRun:     "never",
 			LastLogHash: "none",
 			LastError:   "",
 			State:       Ready,
 		}
-		config.Save()
+		tool.Save()
 	}
-	dat, err := os.ReadFile(config.StateFile)
+	dat, err := os.ReadFile(tool.StateFile)
 	if err != nil {
 		panic(err)
 	}
-	err = json.Unmarshal(dat, &config.State)
+	err = json.Unmarshal(dat, &tool.State)
 	if err != nil {
 		panic(err)
 	}
 }
 
-func (config *Tool) Save() {
-	utils.CheckPathForFile(config.StateFile)
-	encoded, err := json.Marshal(config.State)
+func (tool *Tool) Save() {
+	utils.CheckPathForFile(tool.StateFile)
+	encoded, err := json.Marshal(tool.State)
 	if err != nil {
 		panic(err)
 	}
-	err = os.WriteFile(config.StateFile, encoded, 0644)
+	err = os.WriteFile(tool.StateFile, encoded, 0644)
 	if err != nil {
 		panic(err)
 	}
